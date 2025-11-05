554bdec3b5fa4df718a40a27f5575b37ce44ec1b
TrdrHub Developer <developer@trdrhub.com>
2025-11-05 12:40:35 +0600

    fix: inline cn function in Separator component to fix bundling issue

--- apps/web/src/components/ui/separator.tsx
+++ apps/web/src/components/ui/separator.tsx
@@ -1,7 +1,23 @@
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
-import { cn } from "@/lib/utils"
+// Inline cn function to avoid import/bundling issues
+function cn(...classes: (string | undefined | null | boolean | Record<string, boolean>)[]): string {
+  return classes
+    .filter(Boolean)
+    .map((cls) => {
+      if (typeof cls === 'string') return cls;
+      if (typeof cls === 'object' && cls !== null) {
+        return Object.entries(cls)
+          .filter(([_, val]) => val)
+          .map(([key]) => key)
+          .join(' ');
+      }
+      return '';
+    })
+    .filter(Boolean)
+    .join(' ');
+}
 
 const Separator = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
